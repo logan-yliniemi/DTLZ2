bd900b83b6c0b18ee9555c49ef38042188c94312
Logan Yliniemi <lmy.shp@gmail.com>
2014-02-17 15:16:50 -0800

    dtlz2 main file

---
+++ dtlz2.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ylinieml
+ *
+ * Created on February 17, 2014, 3:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
